660a79ce15b59975d0a5b8a7c609afe9c121d375
Reggem <regis.gemoets@gmail.com>
2019-01-11 17:26:45 +0100

    deleted add comp that was not used

--- JS/addcomp.js
+++
@@ -1,5 +0,0 @@
-function popupwindow() {
-  var left = (screen.width/2)-(200/2);
-  var top = (screen.height/2)-(200/2);
-  return window.open("addcomp.php", "Add a new company", 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+600+', height='+650+', top='+200+', left='+650);
-}
